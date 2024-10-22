987f8f461c7583d2cd942d84150f8b6bf3e22f41
Akshata1234578 <akshata.23cs010@sode-edu.in>
2024-10-22 15:26:09 +0530

    ThradClass

--- A11.java
+++ A11.java
@@ -1,39 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package pkg1;
-class Car{
-    private String make;
-    private String model;
-    private int year;
-    private String color;
-    Car(String mk,String md,int yr,String clr){
-        this.make=mk;
-        this.model=md;
-        this.year=yr;
-        this.color=clr;}
-    
-    public void getCarDetails(){
-        System.out.println(color+""+make+"-"+model+""+year);}}
+//package pkg1
+class MultiThreadingTwo implements Runnable{
+    public void run(){
+        try{
+             System.out.println("Thread "+Thread.currentThread().getId()+" is running");}
+        catch(Exception e){
+            System.out.println("Exception is caught");  
+        }
+             
+    }
+}
 public class A11{
-     public static void main(String[] args){
- Car principalCar=new Car("Hyunda ","i20 ",2022,"White ");
- Car vprincipalCar=new Car("Maruti ","Swift ",2020,"Grey ");
- System.out.println("The two car parked in front of SMVITM are");
- principalCar.getCarDetails();
- vprincipalCar.getCarDetails();}}
- 
- 
- 
- 
-
-
-
-    
-    
-        
-        
-    
-    
+    public static void main(String[] args){
+        for(int i=0;i<8;i++){
+            MultiThreadingTwo mt1=new MultiThreadingTwo();
+            Thread t1=new Thread(mt1);
+            t1.start();
+        }
+    }
+}
