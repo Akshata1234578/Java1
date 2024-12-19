ec33705196c1b4ccd1b165cf5224c18b83a0c75f
Akshata1234578 <akshata.23cs010@sode-edu.in>
2024-12-19 20:51:13 +0530

    Runnable

--- A11.java
+++ A11.java
@@ -1,20 +1,33 @@
-//package pkg1
-class MultiThreadingTwo implements Runnable{
-    public void run(){
-        try{
-             System.out.println("Thread "+Thread.currentThread().getId()+" is running");}
-        catch(Exception e){
-            System.out.println("Exception is caught");  
+package pkg;
+
+import pkg.MyRunnable;
+
+class MyRunnable implements Runnable{
+        @Override
+        public void run() {
+                try {
+                        Thread.sleep(5);
+                        System.out.println("Thread " +Thread.currentThread().getId() + "running");
+                }
+                catch(InterruptedException e) {
+                        e.printStackTrace();
+                }
         }
-             
-    }
 }
 public class A11{
-    public static void main(String[] args){
-        for(int i=0;i<8;i++){
-            MultiThreadingTwo mt1=new MultiThreadingTwo();
-            Thread t1=new Thread(mt1);
-            t1.start();
+
+        public static void main(String[] args) {
+                System.out.println("Runnable");
+                MyRunnable mr1=new MyRunnable();
+                Thread t1=new Thread(mr1);
+                Thread t2=new Thread(mr1);
+                Thread t3=new Thread(mr1);
+                t1.start();
+                t2.start();
+                t3.start();
+
         }
-    }
+
 }
+
+
